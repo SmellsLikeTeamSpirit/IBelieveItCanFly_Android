20300c307b3057e53c4747c12e9633626af7c689
Ilkan Engin <engini@itu.edu.tr>
2015-11-25 22:44:00 +0200

    Thread denemeleri

--- app/src/main/java/com/oia/ilkan/blg456eproject/ControlActivity.java
+++ app/src/main/java/com/oia/ilkan/blg456eproject/ControlActivity.java
@@ -8,18 +8,15 @@ import com.erz.joysticklibrary.JoyStick;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
 public class ControlActivity extends AppCompatActivity implements JoyStick.JoyStickListener{
     JoyStick joyStick1, joyStick2;
     int port;
     String ip;
-    Socket socket;
     SenderThread sender;
-    Double currentAngle, currentPower;
+    Double currentAngle = 0.0, currentPower = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +27,8 @@ public class ControlActivity extends AppCompatActivity implements JoyStick.JoySt
         ip = args.getString("ip");
         joyStick1 = (JoyStick) findViewById(R.id.joy1);
         joyStick2 = (JoyStick) findViewById(R.id.joy2);
-        try {
-            socket = new Socket(ip, port);
-        } catch (IOException e) {
-            System.err.println(e);
-        }
 
-        sender = new SenderThread(socket);
+        sender = new SenderThread();
         sender.start();
 
         joyStick1.setListener(this);
@@ -66,9 +58,15 @@ public class ControlActivity extends AppCompatActivity implements JoyStick.JoySt
             angleToSend = -((3*Math.PI)/2) - angle;
             //joyStick.setButtonColor(Color.parseColor("#696969"));
         }
-        currentAngle = angleToSend;
-        currentPower = power;
-        sender.notify();
+
+
+        synchronized (currentAngle) {
+            synchronized (currentPower) {
+                currentAngle = angleToSend;
+                currentPower = power;
+                sender.notify();
+            }
+        }
 
         Log.d("Joystick On Move", "Power : " + power + " Angle : " + angle + " Angle To Send : " + angleToSend);
     }
@@ -77,26 +75,42 @@ public class ControlActivity extends AppCompatActivity implements JoyStick.JoySt
 
         private Socket hostThreadSocket;
         private OutputStream out;
-        public SenderThread(Socket socket) {
-            hostThreadSocket = socket;
+        private boolean isPortCreated;
+        public SenderThread() {
+            isPortCreated = false;
         }
 
         @Override
         public void run() {
             super.run();
+            if(!isPortCreated) {
+                try {
+                    hostThreadSocket = new Socket(ip, port);
+                    isPortCreated = true;
+                } catch (IOException e) {
+                    System.err.println(e);
+                }
+            }
             try {
                 out = hostThreadSocket.getOutputStream();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            byte[] bytes = new byte[8];
-            ByteBuffer.wrap(bytes).putDouble(currentAngle);
-            ByteBuffer.wrap(bytes).putDouble(currentPower);
-            try {
-                out.write(bytes);
-                out.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
+            byte[] bytes = new byte[16];
+
+            synchronized (currentAngle) {
+                synchronized (currentPower) {
+                    if(currentPower != null && currentAngle != null) {
+                        ByteBuffer.wrap(bytes).putDouble(currentAngle);
+                        ByteBuffer.wrap(bytes).putDouble(currentPower);
+                        try {
+                            out.write(bytes);
+                            out.flush();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
         }
     }
